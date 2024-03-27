e74bda83fdb37b31fe14f1fb74d6917ffa3103ce
Sam Clark <1059176+samhclark@users.noreply.github.com>
2024-03-26 19:55:59 -0500

    Switch to Kong

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "github.com/samhclark/go-cli/cmd"
+import "github.com/alecthomas/kong"
+
+var CLI struct {
+  Rm struct {
+    Force     bool `help:"Force removal."`
+    Recursive bool `help:"Recursively remove files."`
+
+    Paths []string `arg:"" name:"path" help:"Paths to remove." type:"path"`
+  } `cmd:"" help:"Remove files."`
+
+  Ls struct {
+    Paths []string `arg:"" optional:"" name:"path" help:"Paths to list." type:"path"`
+  } `cmd:"" help:"List paths."`
+}
 
 func main() {
-	cmd.Execute()
+  ctx := kong.Parse(&CLI)
+  switch ctx.Command() {
+  case "rm <path>":
+  case "ls":
+  default:
+    panic(ctx.Command())
+  }
 }
